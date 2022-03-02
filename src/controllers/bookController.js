bc130955606295181cb908f866067eb156a6dba9
rhedau1 <rhedau1@gmail.com>
2022-03-03 00:28:30 +0530

    assignment done

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,59 +1,36 @@
 const { count } = require("console")
-const BookModel= require("../models/bookModel")
+const authorModel = require("../models/authorModel")
+const bookModel= require("../models/bookModel")
+const publisherModel = require("../models/publisherModel")
 
 const createBook= async function (req, res) {
-    let data= req.body
+    let book = req.body
+    let authorId = book.author
+    let publisherId = book.publisher
 
-    let savedData= await BookModel.create(data)
-    res.send({msg: savedData})
+    //validation a
+    if(!authorId) return res.send('The request is not valid as the author details are required.')
+
+    //validation b
+    let author = await authorModel.findById(authorId)
+    if(!author) return res.send('The request is not valid as no author is present with the given author id')
+
+    //validation c
+    if(!publisherId) return res.send('The request is not valid as the publisher details are required.') 
+
+    //validation d
+    let publisher = await publisherModel.findById(publisherId)
+    if(!publisher) return res.send('The request is not valid as no publisher is present with the given publisher id')
+
+    let bookCreated = await bookModel.create(book)
+    return res.send({data: bookCreated})
 }
 
-const getBooksData= async function (req, res) {
-    let allBooks= await BookModel.find( {authorName : "HO" } )
-    console.log(allBooks)
-    if (allBooks.length > 0 )  res.send({msg: allBooks, condition: true})
-    else res.send({msg: "No books found" , condition: false})
+const getBooks= async function (req, res) {
+    let books = await bookModel.find().populate('author publisher')
+    res.send({data: books})
 }
-
-
-const updateBooks= async function (req, res) {
-    let data = req.body // {sales: "1200"}
-    // let allBooks= await BookModel.updateMany( 
-    //     { author: "SK"} , //condition
-    //     { $set: data } //update in data
-    //  )
-    let allBooks= await BookModel.findOneAndUpdate( 
-        { authorName: "ABC"} , //condition
-        { $set: data }, //update in data
-        { new: true , upsert: true} ,// new: true - will give you back the updated document // Upsert: it finds and updates the document but if the doc is not found(i.e it does not exist) then it creates a new document i.e UPdate Or inSERT  
-     )
-     
-     res.send( { msg: allBooks})
-}
-
-const deleteBooks= async function (req, res) {
-    // let data = req.body 
-    let allBooks= await BookModel.updateMany( 
-        { authorName: "FI"} , //condition
-        { $set: {isDeleted: true} }, //update in data
-        { new: true } ,
-     )
-     
-     res.send( { msg: allBooks})
-}
-
-
-
-
-// CRUD OPERATIONS:
-// CREATE
-// READ
-// UPDATE
-// DELETE
-
 
 
 module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
-module.exports.updateBooks= updateBooks
-module.exports.deleteBooks= deleteBooks
+module.exports.getBooks= getBooks
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://rhedau1:0115me121110@cluster0.txnwg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
