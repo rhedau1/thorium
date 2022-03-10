c0781f65a36d35d6116ab78bc179fa895551e9d5
rhedau1 <rhedau1@gmail.com>
2022-03-10 09:01:03 +0530

    done

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,34 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+// const authenticate = function(req, res, next) {
+//     //check the token in request header
+//     //validate this token
 
+//     next()
+// }
+
+
+// const authorise = function(req, res, next) {
+//     // comapre the logged in user's id and the id in request
+//     next()
+// }
+
+const jwt =requiew("jsonwebtoken")
+
+let MDwear=function(req, res,next){
+try{
+let token = req.headers["x-auth-token"];
+    if (!token) {
+        return res.send({ status: false, msg: "token must be present" });
+    }
+
+    let decodedToken = jwt.verify(token, "functionup-thorium");
+    
+    if(decodedToken.userId !== req.params.userId){
+        return res.send( { msg : "you must have to login first" } )
+    }
     next()
+       
+   } catch (error){ res.send(error) }   
+       
 }
 
-
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    next()
-}
+module.exports.GB = MDwear
