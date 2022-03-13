ed24cd227247d12a1044114a7ef9716a4fef0353
Rahul Hedau <rhedau1@gmail.com>
2022-03-13 09:14:37 +0530

    assignment done

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -1,4 +1,5 @@
-let axios = require("axios")
+let axios = require("axios");
+const { get } = require("../routes/route");
 
 
 let getStates = async function (req, res) {
@@ -57,10 +58,42 @@ let getByPin = async function (req, res) {
     }
 }
 
+let getDistrictById = async function(req, res)  {
+
+    try {
+        let district_id = req.query.district_id
+        let date = req.query.date
+        console.log(`query params are:${district_id}&${date}`)
+        let options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district_id}&date=${date}`
+
+        }
+        let result = await axios(options)
+        console.log(result.data)
+        res.status(200).send({ msg: result.data })
+
+    } 
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 let getOtp = async function (req, res) {
     try {
         let blahhh = req.body
-        
+
         console.log(`body is : ${blahhh} `)
         var options = {
             method: "post",
@@ -83,3 +116,4 @@ module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.getDistrictById = getDistrictById
